c3eed834157500745c1a3e9f42f84dd1a5294d6d
FreeformSneake <118458886+FreeformSneake@users.noreply.github.com>
2023-04-19 17:41:27 +0200

    Add files via upload

---
+++ komendy.sh
@@ -0,0 +1,60 @@
+git commit
+git commit
+
+git branch bugFix
+git switch bugFix
+
+git branch bugFix
+git switch bugFix
+git commit
+git switch main; git commit
+git merge bugFix
+
+git branch bugFix
+git switch bugFix
+git commit
+git switch main
+git commit
+git switch bugFix
+git rebase main
+
+git switch C4
+
+git switch C3
+
+git switch main
+git branch -f main HEAD~2
+git branch -f main C6
+git switch bugFix
+git branch -f bugFix C0
+git switch C1
+
+git reset HEAD~1
+git switch pushed
+git revert HEAD
+
+git cherry-pick C3 C4 C7
+
+git rebase -1 HEAD~4
+
+git rebase -i main
+git rebase bugFix main
+
+git rebase -i main
+git commit --amend
+git rebase -i main
+git rebase caption main
+
+git switch main
+git cherry-pick C2
+git branch -f main C1
+git cherry-pick C2 C3
+
+git tag v0 C1
+git tag v1 C2
+git switch C2
+
+git switch HEAD~2
+git desribe main
+git switch bugFix
+git commit
